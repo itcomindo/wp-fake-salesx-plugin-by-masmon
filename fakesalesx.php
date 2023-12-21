4187ac8c521c7927e1988564e2fd9f7d34ac6707
Budi Haryono <mail.budiharyono@gmail.com>
2023-12-21 21:05:27 +0700

    ok

--- fakesalesx.php
+++ fakesalesx.php
@@ -3,7 +3,7 @@ defined('ABSPATH') || exit;
 
 /**
  *=========================
- *Plugin Name: Fake Sales X
+ *Plugin Name: MM WP Fake Sales X
  *Plugin URI: https://budiharyono.com/
  *Description: Fake Sales X is a plugin to display fake sales notifications on your website.
  *Version: 2.0.0
